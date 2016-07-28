7a3c81742cf9fd64185452589ccf7c9b63dd989e
Ben Dundee <bdundee@square-root.com>
2016-07-27 21:52:36 -0500

    boilerplate, adding work in calm-bot

--- barracks/calm-bot/run_bot.py
+++ barracks/calm-bot/run_bot.py
@@ -1,3 +1,28 @@
 #!
+from flask import Flask, request, Response
+from barracks.util import get_webhook_secret
 
-import slackclient as sc
+
+app = Flask(__name__)
+
+SLACK_WEBHOOK_SECRET = get_webhook_secret()
+
+
+@app.route('/slack', methods=['POST'])
+def inbound():
+    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+        channel = request.form.get('channel_name')
+        username = request.form.get('user_name')
+        text = request.form.get('text')
+        inbound_message = username + " in " + channel + " says: " + text
+        print(inbound_message)
+    return Response(), 200
+
+
+@app.route('/', methods=['GET'])
+def test():
+    return Response('It works!')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- barracks/util/__init__.py
+++ barracks/util/__init__.py
@@ -1,3 +1,4 @@
 #!
 
-from logging_wrappers import get_bulleted_list, get_canned_header, get_logger, get_default_root_logger
+from .auth import get_api_token, get_webhook_secret
+from .logging_wrappers import get_bulleted_list, get_canned_header, get_logger, get_default_root_logger
