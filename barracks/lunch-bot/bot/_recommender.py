32102c3fd0d5312e46eaf52f3b7fc232a826bad1
jjardel <jjardel@square-root.com>
2016-07-28 17:10:35 -0500

    adding lunch-bot directory

---
+++ barracks/lunch-bot/bot/_recommender.py
@@ -0,0 +1,24 @@
+
+
+class Recommender(object):
+
+    def __init__(self):
+
+        pass
+
+
+    def load(self, data):
+
+        pass
+
+    def build(self):
+
+        # build recommendation & learn weights
+
+        pass
+
+    def recommend(self, user, item):
+
+        # predict a user's recommendation for a given restaurant
+        pass
+
