a35b127ca249f936706d44921fd6bd2f1fd26abf
Ben Dundee <bdundee@square-root.com>
2016-07-28 21:28:29 -0500

    don't need these

---
+++ barracks/calm-bot/bin/env_setup.sh
@@ -0,0 +1,8 @@
+#!
+
+
+SLACK_TOKEN="xoxp-2334828949-3845128274-64147004369-94188995a3",
+SLACK_WEBHOOK_SECRET="WFoQyt5m3iwKqu9ieXZ5AaKu"
+
+# Run
+../../env/python-3.5/src/py35/bin/python ../run_bot.py
---
+++ templates/python-bot/bin/env_setup.sh
@@ -0,0 +1,9 @@
+#!
+
+######
+
+#####
+
+## Basic access tokens
+heroku config:set SLACK_TOKEN=""
+heroku config:set SLACK_WEBHOOK_SECRET=""
